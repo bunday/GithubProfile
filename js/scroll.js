a291f6adb3f7d3e98f8703aa44f91c35a29adc67
Zadat Olayinka <bundayy@outlook.com>
2020-11-14 10:24:25 +0100

    hide and show small header

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,22 @@
-window.onscroll = function() {repositionHeader()};
+window.onscroll = function () {
+  repositionHeader();
+};
 const staticHeader = document.getElementById("static-header");
 const sticky = staticHeader.offsetTop;
+const userDetails = document.getElementById("userDetails");
+const smallUserInfo = document.getElementById("smallUserInfo");
 
 function repositionHeader() {
   if (window.pageYOffset > sticky) {
     staticHeader.classList.add("sticky");
   } else {
     staticHeader.classList.remove("sticky");
+  }
+  if (userDetails.getBoundingClientRect().y < 0) {
+    smallUserInfo.classList.add("show");
+    smallUserInfo.classList.remove("hide");
+  } else {
+    smallUserInfo.classList.add("hide");
+    smallUserInfo.classList.remove("show");
   }
 }
